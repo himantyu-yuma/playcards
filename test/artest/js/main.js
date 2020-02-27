c1ee55159189d53a24c5b8016fa507209b436a76
aroe <lipton.happy.milktea@gmail.com>
2020-02-27 16:29:54 +0900

    debug

--- test/artest/js/main.js
+++ test/artest/js/main.js
@@ -1,26 +1,19 @@
-function detectMarker() {
-    AFRAME.registerComponent('registerevents', {
-        init: function () {
-            var marker = this.el;
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        var marker = this.el;
 
-            // マーカーを検出したイベントの登録
-            marker.addEventListener('markerFound', function () {
-                var markerId = marker.id;
-                console.log('markerFound', markerId);
+        // マーカーを検出したイベントの登録
+        marker.addEventListener('markerFound', function () {
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
 
-                // アニメーションの開始
-                document.querySelector('#box').emit('markerfound');
-            });
+        });
 
-            // マーカーを見失ったイベントの登録
-            marker.addEventListener('markerLost', function () {
-                var markerId = marker.id;
-                console.log('markerLost', markerId);
+        // マーカーを見失ったイベントの登録
+        marker.addEventListener('markerLost', function () {
+            var markerId = marker.id;
+            console.log('markerLost', markerId);
 
-                // アニメーションの停止
-                document.querySelector('#box').emit('markerlost');
-            });
-        }
-    });
-}
-detectMarker();
+        });
+    }
+});
